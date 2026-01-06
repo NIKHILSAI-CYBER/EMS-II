27942678fec786aa5565a1cada03a05bb9c2b049
NIKHILSAI-CYBER <nikhilsai9505@gmail.com>
2026-01-06 12:39:03 +0530

    Employee update and delete APIs

--- account/serializers.py
+++ account/serializers.py
@@ -1,14 +1,48 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User
 from django.utils.crypto import get_random_string
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ("password",)
 
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(
+            email=attrs["email"],
+            password=attrs["password"]
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid email or password"
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                "User account is disabled"
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+            "role": user.role,
+            # "email": user.email,
+            # "full_name": user.full_name,
+            # "is_first_login": user.is_first_login,
+        }
 
 class CreateEmployeeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -43,6 +77,25 @@ class CreateEmployeeSerializer(serializers.ModelSerializer):
 
         return user
     
+
+class UpdateEmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("full_name", "department", "role", "is_active")
+
+
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField()
     new_password = serializers.CharField()
+
+# class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+
+#         token["role"] = user.role
+#         token["email"] = user.email
+#         token["full_name"] = user.full_name
+
+#         return token
--- account/urls.py
+++ account/urls.py
@@ -5,13 +5,23 @@ from .views import *
 urlpatterns = [
     
     path("signup/", SignupView.as_view()),
-    path("login/", TokenObtainPairView.as_view()),
+    path("login/", LoginView.as_view()),
+    # path("token/refresh/", TokenRefreshView.as_view()),
+    # path("login/", CustomTokenObtainPairView.as_view()),/
+
     path("token/refresh/", TokenRefreshView.as_view()),
 
     path("dashboard/", DashboardView.as_view()),
     path("employees/", EmployeeListView.as_view()),
+    path("employees/<int:id>/",EmployeeDetailView.as_view()),
     path("profile/", ProfileView.as_view()),
     path("change-password/", ChangePasswordView.as_view()),
 
     path("admin/create-employee/", CreateEmployeeView.as_view()),
+    path("admin/employees/<int:id>/",DeleteEmployeeView.as_view()),
+    path("admin/employees/<int:id>/",UpdateEmployeeView.as_view()),
+    # path("admin/employees/<int:id>/", EmployeeAdminView.as_view()),
+    
+    
+
 ]
