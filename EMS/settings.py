277a5fdbca33675cd35a8564bb698efd347a3f4f
swethadomatoti <swethadomatoti@gmil.com>
2026-02-09 16:30:19 +0530

    Changes in settings

--- EMS/settings.py
+++ EMS/settings.py
@@ -88,7 +88,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "swethadomatoti@gmail.com"
-EMAIL_HOST_PASSWORD = "vzqvbbtmzvexxgdz"
+EMAIL_HOST_PASSWORD = "kwuetenslwqwuolj"
 DEFAULT_FROM_EMAIL = "EMS <swethadomatoti@gmail.com>"
 
 from datetime import timedelta
